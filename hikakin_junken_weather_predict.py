f13a7f20ec35f7304c35a1708afcbc7f41cce135
daiki <dadada7772000@yahoo.co.jp>
2021-11-30 16:28:11 +0900

    じゃんけん予測プログラムをjunken_train_operation4に変更

--- hikakin_junken_weather_predict.py
+++ hikakin_junken_weather_predict.py
@@ -1,9 +1,9 @@
 # coding:utf-8
-from data.data_parse_write5 import data_parse_write5
-from train.junken_train_operation5 import junken_train_operation5
+from data.data_parse_write4 import data_parse_write4
+from train.junken_train_operation4 import junken_train_operation4
 
 if __name__ == "__main__":
     # じゃんけん予測のためのcsvデータを用意
-    data_parse_write5()
+    data_parse_write4()
     # csvデータから本日のじゃんけんを予測し、Mysqlに登録
-    junken_train_operation5()
+    junken_train_operation4()
