269fe255faa14a8610a280df045098d3e34f1de6
daiki <dadada7772000@yahoo.co.jp>
2022-01-20 18:32:22 +0900

    ジャンケンの予測結果が「パー」が続いてしまうため、 ランダムにジャンケンの手を予測するように変更

--- hikakin_junken_weather_predict.py
+++ hikakin_junken_weather_predict.py
@@ -1,9 +1,13 @@
 # coding:utf-8
 from data.data_parse_write4 import data_parse_write4
 from train.junken_train_operation4 import junken_train_operation4
+from tools.junken_random import junken_random
 
 if __name__ == "__main__":
-    # じゃんけん予測のためのcsvデータを用意
-    data_parse_write4()
-    # csvデータから本日のじゃんけんを予測し、Mysqlに登録
-    junken_train_operation4()
+    # # じゃんけん予測のためのcsvデータを用意
+    # data_parse_write4()
+    # # csvデータから本日のじゃんけんを予測し、Mysqlに登録
+    # junken_train_operation4()
+
+    # じゃんけんをランダムで予測し、Mysqlに登録
+    junken_random()
